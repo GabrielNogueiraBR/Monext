2c822caeaa757867c8b2005f068e49852aa24619
Vitor Gois <vitorcgois777@gmail.com>
2021-10-03 19:23:15 -0300

    chore: remove unnecessary nodemon configs in package.json and fix require in server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-const express = reuire('express');
+const express = require('express');
 const path = require('path');
 
 // eslint-disable-next-line no-underscore-dangle
-const __dirname = path.resolve();
+const dirname = path.resolve();
 const app = express();
 const port = 3000;
 
-app.use(express.static(`${__dirname}/src/views`));
+app.use(express.static(`${dirname}/src/views`));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
