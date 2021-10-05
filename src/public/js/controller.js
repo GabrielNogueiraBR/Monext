8a02d9b9f03153a55a2b29f79d88de33351cfc40
= <=>
2021-10-05 00:48:19 -0300

    commit

--- src/public/js/controller.js
+++ src/public/js/controller.js
@@ -12,7 +12,22 @@ $('.button-left').on('click', (event) => {
   atualizaController(teste);
 });
 
+$('.button-big-left').on('click', (event) => {
+  teste -= 2;
+  atualizaController(teste);
+});
+
 $('.button-right').on('click', (event) => {
   teste += 1;
+  atualizaController(teste);
+});
+
+$('.button-big-right').on('click', (event) => {
+  teste += 2;
+  atualizaController(teste);
+});
+
+$('.button-home').on('click', (event) => {
+  teste *= 0;
   atualizaController(teste);
 });
