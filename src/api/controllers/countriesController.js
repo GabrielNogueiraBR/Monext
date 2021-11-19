9bb5cded0882a5144cedf7e4d1de532f5d8853b6
Vitor Gois <vitorcgois777@gmail.com>
2021-11-18 22:12:59 -0300

    chore: remove unused controller

--- src/api/controllers/countriesController.js
+++ src/api/controllers/countriesController.js
@@ -1,47 +1,9 @@
 /* eslint-disable class-methods-use-this */
-const moment = require('moment-timezone');
 const { CountriesService } = require('../services/countriesService');
-const FetchDataService = require('../helpers/fetchDataService');
 
 const countriesService = new CountriesService();
 
 class CountriesController {
-  async send(req, res) {
-    const {
-      baseCurrency, targetCurrency, capital, country,
-    } = req.body;
-
-    const fetchService = new FetchDataService();
-
-    try {
-      // The order here to call external services matters,
-      // because we are using two features of the same API (fetch exchange and timezone API),
-      // and if we send one after the other we get error 429 (many requests in a row),
-      // needing to have a "interval" between requests,
-      // this "interval" we are using a request for an API from another domain.
-      const exchange = await fetchService.fetchExchangeApi(baseCurrency, targetCurrency);
-      const countriesInfo = await fetchService.fetchCountriesInfoApi();
-      // example coming from request body (capital and country) for fetch timezone
-      const timezone = await fetchService.fetchTimezoneApi(capital, country);
-      const weather = await fetchService.fetchWeatherApi(capital);
-
-      // Example generating current time with GMT of other countries specifying
-      // with timezone_location coming from timezone API.
-      const tz = moment().tz(timezone.timezone_location).format();
-      console.log(tz);
-
-      res.status(201).json({
-        countriesInfo,
-        exchange,
-        timezone,
-        weather,
-      });
-    } catch (err) {
-      console.log(err);
-      res.sendStatus(500);
-    }
-  }
-
   async create(req, res) {
     try {
       // Receiving country and value of conversion
