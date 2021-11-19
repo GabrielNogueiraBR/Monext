32990aaa8f9eeee25e41c51a619e26f657fdf259
Vitor Gois <vitorcgois777@gmail.com>
2021-11-18 22:30:43 -0300

    fix: countries capital gmt

--- src/api/mock/timezone.js
+++ src/api/mock/timezone.js
@@ -2,62 +2,52 @@ const timezoneMocked = [
   {
     countryName: 'Argentina',
     capital: 'Buenos Aires',
-    timezone: 'America/Argentina/Buenos_Aires',
     gmt_offset: '-3',
   },
   {
     countryName: 'Brazil',
     capital: 'Brasilia',
-    timezone: 'America/Sao_Paulo',
     gmt_offset: '-3',
   },
   {
     countryName: 'Canada',
     capital: 'Ottawa',
-    timezone: 'America/Toronto',
     gmt_offset: '-4',
   },
   {
     countryName: 'China',
     capital: 'Beijing',
-    timezone: 'Asia/Shanghai',
     gmt_offset: '+8',
   },
   {
     countryName: 'Italy',
     capital: 'Rome',
-    timezone: 'Europe/Rome',
-    gmt_offset: '+2',
+    gmt_offset: '+1',
   },
   {
     countryName: 'Japan',
     capital: 'Tokyo',
-    timezone: 'Asia/Tokyo',
     gmt_offset: '+9',
   },
   {
     countryName: 'Mexico',
     capital: 'Mexico City',
-    timezone: 'America/Mexico_City"',
-    gmt_offset: '-5',
+    gmt_offset: '-6',
   },
   {
     countryName: 'Russia',
     capital: 'Moscow',
-    timezone: 'Europe/Moscow',
     gmt_offset: '+3',
   },
   {
     countryName: 'United Kingdom',
     capital: 'London',
-    timezone: 'Europe/London',
-    gmt_offset: '+1',
+    gmt_offset: '+0',
   },
   {
     countryName: 'United States',
     capital: 'Washington',
-    timezone: 'America/New_York',
-    gmt_offset: '-4',
+    gmt_offset: '-5',
   },
 ];
 
