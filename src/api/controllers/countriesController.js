ede1d9fa01e544fc39ce713af026bbb95e0b5e49
Vitor Gois <vitorcgois777@gmail.com>
2021-11-24 20:48:20 -0300

    chore: JSDoc

--- src/api/controllers/countriesController.js
+++ src/api/controllers/countriesController.js
@@ -3,19 +3,29 @@ const { CountriesService } = require('../services/countriesService');
 
 const countriesService = new CountriesService();
 
+/**
+ * Controller resposible to handle requests
+ * to build country data.
+ */
 class CountriesController {
+  /**
+   * Create countries data.
+   * @param {Request} req
+   * @param {Response} res
+   */
   async create(req, res) {
     try {
       // Receiving country and value of conversion
       const { country, valueConversion } = req.body;
 
+      // Access countries service to create all countries based on requested data.
       const countries = await countriesService.createAllCountries(country, valueConversion);
 
       // Send response (Array of countries)
       res.send(countries);
     } catch (err) {
-      console.error(err);
-      res.status(500).send({ error: err }); // TODO: WHY NOT SEND THE ERROR OBJECT?????
+      console.error(err); // If has error, print in console.
+      res.status(500).send({ error: err }); // Send error 500.
     }
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const { http } = require('./http');
 require('./api/websocket/socketServer');
 
+// Look for port in the environment variables and as a fallback we have port 3000
 const port = process.env.PORT || 3000;
 
+// Server will listen on the port defined in line 5 and will listen for requests
 http.listen(port, () => {
   console.log('listening on *:', port);
 });
