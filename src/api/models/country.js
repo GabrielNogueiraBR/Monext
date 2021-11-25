cc147b3e25d83775daf38dd3c583c9d048c42976
Gabriel José Ferraz Perin <contato.gferraz@gmail.com>
2021-11-24 21:08:23 -0300

    chore: JSDoc country.js

--- src/api/models/country.js
+++ src/api/models/country.js
@@ -1,3 +1,18 @@
+/**
+ * Class responsible for constructing the main entity of the project.
+ *
+ * @param {object} flag Offset to be send flag country.
+ * @param {string} name Offset to be send name country.
+ * @param {number} exchange Offset to be send conversion money.
+ * @param {string} currencyAcronym Offset to be send  currency acronym of country.
+ * @param {string} currencyName Offset to be send currency name of country.
+ * @param {number} date Offset to be send country date.
+ * @param {number} gmt Offset to be send GMT value for country time conversion.
+ * @param {number} temperature Offset to be send temperature country.
+ * @param {string} capital Offset to be send capital country.
+ *
+ * @author Gabriel Perin
+ **/
 class Country {
   constructor(name, capital, currencyAcronym, currencyName, exchange, flag, date, gmt, temperature) {
     this.flag = flag;
